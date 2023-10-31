9f3a484db23ab9f81543e88ac5a24523bff43cce
unknown <thaitrongtindev@gmail.com>
2023-10-31 21:00:41 +0700

    handle create account by firebaseAuth

---
+++ app/src/main/java/com/example/socialmedia/FragmentReplaceActivity.java
@@ -0,0 +1,14 @@
+package com.example.socialmedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentReplaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_replace);
+    }
+}
